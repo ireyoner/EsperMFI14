dac9299954788d561e07fc5633d3e76143385e13
Ireyoner <92misiu@gmail.com>
2015-04-30 15:25:32 +0200

    Small fix + counting min/max

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,16 @@ public class Main {
         EPServiceProvider serviceProvider = EPServiceProviderManager.getDefaultProvider();
         EPAdministrator administrator = serviceProvider.getEPAdministrator();
 
-        administrator.createEPL(""
-        		+ "create window for14days.std:groupwin(spolka).win:length(14)"
-        		+ "  (data Date"
-        		+ "  ,spolka String"
-        		+ "  ,obrotForsyPlus Double"
-        		+ "  ,obrotForsyMinus Double"
-        		+ "  ,kursDywergencji Double"
-//        		+ "  ,kursSredni Double"
-        		+ "  )");
+        // Wyliczenie wartosci do wyliczania MFI14
+//        administrator.createEPL(""
+//        		+ "create window for14days.std:groupwin(spolka).win:length(14)"
+//        		+ "  (data Date"
+//        		+ "  ,spolka String"
+//        		+ "  ,obrotForsyPlus Double"
+//        		+ "  ,obrotForsyMinus Double"
+//        		+ "  ,kursDywergencji Double"
+////        		+ "  ,kursSredni Double"
+//        		+ "  )");
         administrator.createEPL(""
         		+ "insert into for14days "
         		+ "select data as data"
@@ -31,15 +32,16 @@ public class Main {
 //        		+ "     , (wartoscMax + wartoscMin + kursZamkniecia) / 3 as kursSredni"
         		+ "  from KursAkcji.std:groupwin(spolka).win:length(2)");
 
+        // Wyliczenie MFI14 (dla 3 dni, zeby szukac max i min)
+//        administrator.createEPL(""
+//        		+ "create window for3days.std:groupwin(spolka).win:length(3)"
+//        		+ "  (data Date"
+//        		+ "  ,spolka String"
+//        		+ "  ,MFI_14 Double"
+//        		+ "  ,kursDywergencji Double"
+//        		+ "  )");
         administrator.createEPL(""
-        		+ "create window for3days.std:groupwin(spolka).win:length(3)"
-        		+ "  (data Date"
-        		+ "  ,spolka String"
-        		+ "  ,MFI_14 Double"
-        		+ "  ,kursDywergencji Double"
-        		+ "  )");
-        administrator.createEPL(""
-        		+ "insert into for3days"
+        		+ "insert into for3days "
         		+ "select data as data"
         		+ "     , spolka as spolka"
         		+ "     , 100d - ( 100d / ( 1d "
@@ -47,10 +49,32 @@ public class Main {
               + "                   /  (sum(obrotForsyMinus)) )"
               + "                   )) as MFI_14"
               + "       , kursDywergencji as kursDywergencji  "
-        		+ "  from for14days group by spolka");
+        		+ "  from for14days.std:groupwin(spolka).win:length(14) group by spolka");
+        
         EPStatement statement = administrator.createEPL(""
-        		+ "select *  "
-        		+ "  from for3days ");
+        		+ "select f3d.data as data"
+        		+ "     , f3d.spolka as spolka"
+        		+ "     , f3d.MFI_14 as MFI_14_0"
+        		+ "     , prev(1, f3d.MFI_14) as MFI_14_1"
+        		+ "     , prev(2, f3d.MFI_14) as MFI_14_2"
+        		+ "     , f3d.kursDywergencji as kursDywergencji_0"
+        		+ "     , prev(1, f3d.kursDywergencji) as kursDywergencji_1"
+        		+ "     , prev(2, f3d.kursDywergencji) as kursDywergencji_2"
+        		+ "     , case when prev(2, f3d.kursDywergencji) > prev(1, f3d.kursDywergencji) "
+        		+ "             and prev(1, f3d.kursDywergencji) < f3d.kursDywergencji "
+        		+ "            then 'min' "
+        		+ "            when prev(2, f3d.kursDywergencji) < prev(1, f3d.kursDywergencji) "
+        		+ "             and prev(1, f3d.kursDywergencji) > f3d.kursDywergencji "
+        		+ "            then 'max' "
+        		+ "            else 'brak' end as wspDyw "
+        		+ "     , case when prev(2, f3d.MFI_14) > prev(1, f3d.MFI_14) "
+        		+ "             and prev(1, f3d.MFI_14) < f3d.MFI_14 "
+        		+ "            then 'min' "
+        		+ "            when prev(2, f3d.MFI_14) < prev(1, f3d.MFI_14) "
+        		+ "             and prev(1, f3d.MFI_14) > f3d.MFI_14 "
+        		+ "            then 'max' "
+        		+ "            else 'brak' end as wspMFI"
+        		+ "  from for3days.std:groupwin(spolka).win:length(3) f3d");
 
       ProstyListener listener = new ProstyListener();
       statement.addListener(listener);
